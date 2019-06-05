d174ada314e91379f3b0f1d0c2e58e135cc2976e
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-06-05 14:56:12 -0700

    pausing and starting gifs

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,6 +5,7 @@ var topics = ["tennis", "Grigor Dimitrov", "cars", "Volvo", "animals", "dancing"
 
 // set a function to render the buttons
 function renderButtons() {
+    // clear content in btn-container so that the div get cleared (prevents the div to over-populate with replicated buttons)
     $("#btn-container").empty();
     for (var i = 0; i < topics.length; i++) {
         console.log(topics[i]);
@@ -34,9 +35,19 @@ function getAndDisplayGifs(name) {
         for (var i = 0; i < gifs.length; i++) {
             var image = $("<img>");
             image.addClass("giphy-image");
-            image.attr("src", gifs[i].images.original.url);
+            image.attr("src", gifs[i].images.original_still.url);
             image.attr("alt", name);
+            var imageStill = gifs[i].images.original_still.url;
+            var imagesAnimated = gifs[i].images.original.url;
+            // assign attributes with those urls from our stored variables
+            image.attr({
+                "data-still": imageStill,
+                "data-state": "still",
+                "data-animate": imagesAnimated
+            });
             $("#gif-container").append(image);
+            // create a new varibles for both still and animated links
+
         }
     });
 }
@@ -55,28 +66,49 @@ $(document).on("click", ".topics-btn", function () {
 
 $("#find-topic").on("click", function (event) {
 
-// Preventing the submit button from trying to submit the form
-// We're optionally using a form so the user may hit Enter to search instead of clicking the button
+    // Preventing the submit button from trying to submit the form
+    // We're optionally using a form so the user may hit Enter to search instead of clicking the button
     event.preventDefault();
 
-// Here we grab the text from the input box
+    // Here we grab the text from the input box
     var topic = $("#topic-input").val();
 
-// Write code  to hit the queryURL with $.ajax, then take the response data
-// and display it in the gif-container div 
+    // Write code  to hit the queryURL with $.ajax, then take the response data
+    // and display it in the gif-container div 
 
     topics.push(topic);
     console.log(topics);
     renderButtons();
 
-   getAndDisplayGifs(topic);
+    getAndDisplayGifs(topic);
+
+    // IMPORTANT! AN OBJECT IS NOT A STRING; 
+    // IF YOU WANT TO DISPLAY SOMETHING ON THE PAGE, IT HAS TO BE A STRING
+    // THAT IS WHY WE USE JSON.STRINGIFY TO CONVERt THE OBJECT INTO A STRING 
+    // $("#movie-view").text(response); RESULTS TO [object Object]
+
+    // $("#gif-container").html(JSON.stringify(response));
+});
 
 
-        // IMPORTANT! AN OBJECT IS NOT A STRING; 
-        // IF YOU WANT TO DISPLAY SOMETHING ON THE PAGE, IT HAS TO BE A STRING
-        // THAT IS WHY WE USE JSON.STRINGIFY TO CONVERt THE OBJECT INTO A STRING 
-        // $("#movie-view").text(response); RESULTS TO [object Object]
 
-        $("#gif-container").html(JSON.stringify(response));
-    });
 
+// add ratings to show on the page under each gif
+
+
+// add PAUSING functionality 
+$(document).on("click", ".giphy-image", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
